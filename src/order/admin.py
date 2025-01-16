d4f0c36b7e7efaa03964076b88f02f652a6795fe
Mykola <kolia.chaun@icloud.com>
2025-01-16 13:17:36 +0200

    Implemented search and made minor improvements.

--- src/order/admin.py
+++ src/order/admin.py
@@ -1,4 +1,3 @@
-# Register your models here.
 from django.contrib import admin
 
 from order.models import Basket, BasketItem
@@ -9,12 +8,18 @@ class BasketAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "created_at", "updated_at")
     list_filter = ("user",)
     date_hierarchy = "created_at"
-    search_fields = ("id", "user")
+    search_fields = (
+        "id",
+        "user__username",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
 
 
 @admin.register(BasketItem)
 class BasketItemAdmin(admin.ModelAdmin):
     list_display = ("basket", "product", "color", "size", "quantity", "created_at")
-    list_filter = ("basket", "product", "color", "size", "quantity")
+    list_filter = ("color", "size", "quantity")
     date_hierarchy = "created_at"
-    search_fields = ("basket", "product", "color", "size", "quantity")
+    search_fields = ("basket__id", "product__title", "color__title", "size__value")
